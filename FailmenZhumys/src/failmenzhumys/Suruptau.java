145347867b7703300bae3739472acdbcd122f651
SShalkharr <kozhabay2002@gmail.com>
2021-12-09 22:44:49 +0600

    2 zhana nuskasy

---
+++ FailmenZhumys/src/failmenzhumys/Suruptau.java
@@ -0,0 +1,11 @@
+package failmenzhumys;
+import java.util.Comparator;
+
+public class Suruptau implements Comparator<Derekter>{
+     @Override
+   public int compare(Derekter o1, Derekter o2) {
+    
+       return (o1.getfam()).compareToIgnoreCase(o2.getfam());
+   }
+    
+}
